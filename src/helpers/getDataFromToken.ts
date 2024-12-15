1b1c071be67bbe19a96103cf3ae9f24d96765166
Dhvanit <monparadhvanit@gmail.com>
2024-12-15 11:16:08 +0530

    fixed middlerware

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -13,20 +13,17 @@ export const getDataFromToken = (req: NextRequest) => {
       throw new Error("Token not found");
     }
 
-    // console.log(token);
-
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload | DecodedToken;
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as
+      | JwtPayload
+      | DecodedToken;
 
     if (typeof decodedToken === "object" && "id" in decodedToken) {
-      // Log specific details rather than the whole object to avoid circular references.
-      // console.log(`Decoded token ID: ${decodedToken.id}`);
       return decodedToken.id;
     } else {
       throw new Error("Invalid token structure");
     }
   } catch (error: unknown) {
     if (error instanceof Error) {
-      // Avoid converting the entire error object to JSON if it has circular references.
       throw new Error(`Error decoding token: ${error.message}`);
     } else {
       throw new Error("An unknown error occurred while decoding the token.");
